a3239f07c1508c170bf7bbd9600b65d23879c33b
Cristian Jara C <cr.jara@duocuc.cl>
2022-06-28 00:27:25 -0400

    API Rest
    v1

---
+++ ProyectoMascotaFeliz/rest_cliente/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from app.models import Cliente
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['idCliente', 'nombreCliente', 'apellido', 'comuna']
